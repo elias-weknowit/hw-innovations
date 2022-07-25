447027cbb5640cc5bb7c39f3edc4ee7570d03fbb
Reza Parvar <reza.parvar@weknowit.nu>
2022-07-25 17:17:34 +0200

    Fixed a minor issue

--- components/Login/InputArea.tsx
+++ components/Login/InputArea.tsx
@@ -6,9 +6,9 @@ export default function InputArea({ placeholder, type }) {
     <div>
       <div className="flex w-1/2 justify-start ml-36 transition duration-150 ease-in-out focus-within:border-primary mb-4">
         <div className="w-full relative flex items-center">
-          <MailIcon className="w-6 h-6 absolute ml-2 pointer-events-none" />
+          <MailIcon className="w-6 h-6 absolute ml-3 pointer-events-none" />
           <input
-            className="w-full px-10 p-2 text-primary outline-none text-base font-light rounded-xl border-none ring-1 ring-white focus:ring-primary-color"
+            className="w-full px-12 h-14 text-primary outline-none text-base font-light rounded-xl border-none ring-1 ring-white focus:ring-primary-color"
             type={type}
             placeholder={placeholder}
           />
