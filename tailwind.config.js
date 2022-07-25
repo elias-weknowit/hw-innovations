1e2c0e6247ced0c20fcd4196677d38df79f127be
Reza Parvar <reza.parvar@weknowit.nu>
2022-07-25 17:05:23 +0200

    Added colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "background-white-color": "#F7F7F7",
+      "primary-color": "#8467AA",
+      "secondary-color": "#EB363D",
+      white: "#fff",
+      black: "#000",
+    },
+    fontFamily: {
+      mulish: "font-family: 'Mulish', sans-serif",
+    },
     extend: {},
   },
   plugins: [],
