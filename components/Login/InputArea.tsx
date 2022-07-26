710dff0aca888db7b92ef4694efc4fc89635a43b
rasmuslyxell <rasmus.lyxell@weknowit.se>
2022-07-26 13:56:10 +0200

    Changed rounded property to medium to match figma

--- components/Login/InputArea.tsx
+++ components/Login/InputArea.tsx
@@ -8,7 +8,7 @@ export default function InputArea({ placeholder, type }) {
         <div className="w-full relative flex items-center">
           <MailIcon className="w-6 h-6 absolute ml-3 pointer-events-none" />
           <input
-            className="w-full px-12 h-14 text-primary outline-none text-base font-light rounded-xl border-none ring-1 ring-white focus:ring-primary-color"
+            className="w-full px-12 h-14 text-primary outline-none text-base font-light rounded-md border-none ring-1 ring-white focus:ring-primary-color"
             type={type}
             placeholder={placeholder}
           />
--- components/Login/LoginButton.tsx
+++ components/Login/LoginButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function LoginButton({ title }) {
   return (
-    <button className="w-80 justify-start mt-2 rounded-xl h-14 bg-primary-color">
+    <button className="w-80 justify-start mt-2 rounded-md h-14 bg-primary-color">
       <p className="content-center text-white text-lg font-mulish">{title}</p>
     </button>
   );
