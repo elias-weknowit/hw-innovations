58d761dca2350b1747f198f19b77ecf7063e0c63
rasmuslyxell <rasmus.lyxell@weknowit.se>
2022-07-26 14:40:09 +0200

    Created component for alternate login buttons and added things missing from figma

--- pages/login/LoginView.tsx
+++ pages/login/LoginView.tsx
@@ -3,7 +3,9 @@ import React from "react";
 import Image from "next/image";
 import logo from "../../public/logo.svg";
 import Loginform from "../../components/Login/Loginform";
+import AlternateLogins from "../../components/Login/AlternateLogins";
 import landing_img from "../../public/manWorking.svg";
+import { Divider } from '@mui/material';
 
 export default function LoginView() {
   return (
@@ -24,11 +26,19 @@ export default function LoginView() {
         </div>
 
         <div className="flex flex-2 flex-grow flex-col w-auto items-center justify-center">
-          <div className="p-6">
+          <div className="p-6 flex flex-col">
             <div className="flex m-0 mt-6 justify-center">
               <Image src={logo} width={370} height={123} />
             </div>
             <Loginform />
+            <a href="" className="m-8 self-center text-light-text">Glömt lösenord?</a>
+            <div>
+              <Divider className='text-light-text'>Eller fortsätt med</Divider>
+            </div>
+           
+
+            <AlternateLogins className="p-6" enableApple enableFacebook enableGoogle/>
+            <p className="self-center text-light-text">Har du inget konto? <a href="" className='text-primary-color'>Registrera</a></p>
           </div>
         </div>
       </div>
