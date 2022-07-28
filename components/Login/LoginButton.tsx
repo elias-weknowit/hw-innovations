e8809ef85af047bc4fae85529b98e1c561aede6b
rasmuslyxell <rasmus.lyxell@weknowit.se>
2022-07-28 16:45:22 +0200

    Added onclick prop

--- components/Login/LoginButton.tsx
+++ components/Login/LoginButton.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function LoginButton({ title }) {
+export default function LoginButton({ title, onClick }: { title: string, onClick: () => void }) {
   return (
-    <button className="w-80 justify-start mt-2 rounded-md h-14 bg-primary-color">
+    <button onClick={onClick} className="w-80 justify-start mt-2 rounded-md h-14 bg-primary-color">
       <p className="content-center text-white text-lg font-mulish">{title}</p>
     </button>
   );
