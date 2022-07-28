3a0c87c1033440183744e42c075da57a9797a7c9
rasmuslyxell <rasmus.lyxell@weknowit.se>
2022-07-28 16:45:48 +0200

    Remove imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,3 @@
-import { getAuth } from "firebase/auth";
-import FirebaseProvider from "../components/firebase/FirebaseProvider";
-import InputArea from "../components/Login/InputArea";
 import LoginView from "./login/LoginView";
 
 export default function Home() {
