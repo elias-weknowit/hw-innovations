45f2964ad09f4e43ce0f3d9f2c1880a26719b73f
rasmuslyxell <rasmus.lyxell@weknowit.se>
2022-07-29 09:32:45 +0200

    Added google redirect login

--- components/Login/AlternateLogins.tsx
+++ components/Login/AlternateLogins.tsx
@@ -2,6 +2,7 @@ import { IconButton, SvgIcon } from "@mui/material";
 import GoogleIcon from "@mui/icons-material/Google";
 import AppleIcon from "@mui/icons-material/Apple";
 import FacebookIcon from "@mui/icons-material/Facebook";
+import { useAuth } from "../../components/firebase/AuthUserProvider";
 import MailIcon from "@mui/icons-material/Mail";
 import Link from "next/link";
 
@@ -12,28 +13,24 @@ export default function AlternateLogins({
   enableFacebook = false,
   enableMail = false,
 }) {
+  const { signInWithGoogleRedirect } = useAuth();
+
   return (
     <div className={className}>
       <div className="flex flex-row justify-evenly ">
-        {enableGoogle && (
-          <IconButton className="bg-white drop-shadow" size="large">
-            <Link href="/">
+        {enableApple && (
+          <IconButton className="bg-white drop-shadow" size="large" >
               <SvgIcon component={AppleIcon} inheritViewBox />
-            </Link>
           </IconButton>
         )}
-        {enableApple && (
-          <IconButton className="bg-white drop-shadow" size="large">
-            <Link href="/">
+        {enableGoogle && (
+          <IconButton className="bg-white drop-shadow" size="large" onClick={signInWithGoogleRedirect}>
               <SvgIcon component={GoogleIcon} inheritViewBox />
-            </Link>
           </IconButton>
         )}
         {enableFacebook && (
           <IconButton className="bg-white drop-shadow" size="large">
-            <Link href="/">
               <SvgIcon component={FacebookIcon} inheritViewBox />
-            </Link>
           </IconButton>
         )}
         {enableMail && (
