66abab86a9442d3454d5f5f167d444e1256e1ca7
rasmuslyxell <rasmus.lyxell@weknowit.se>
2022-08-01 14:42:35 +0200

    Naming fix

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { useState } from "react";
 import Image from "next/image";
-import logo from "../public/logo.svg";
+import logo from "../public/Logo.svg";
 import Loginform from "../components/Login/Loginform";
 import AlternateLogins from "../components/Login/AlternateLogins";
 import landing_img from "../public/LoginImg.png";
