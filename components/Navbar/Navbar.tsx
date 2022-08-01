5e304298b221ff7201091e468731735573841a63
rasmuslyxell <rasmus.lyxell@weknowit.se>
2022-08-01 14:37:20 +0200

    Moved Navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Image from "next/image";
-import logo from "../public/Logo.svg";
+import logo from "../../public/Logo.svg";
 import Link from "next/link";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import ProfilePreview from "./ProfilePreview";
+import LoginImg from "../../public/LoginImg.png";
 
 export default function Navbar() {
   return (
@@ -26,20 +27,7 @@ export default function Navbar() {
           </p>
         </Link>
       </div>
-      <div className="md:m-32 flex flex-row items-center">
-        <div className="flex bg-black bg-opacity-40 rounded-full w-11 h-11 items-center justify-center text-white">
-          img
-        </div>
-        <div className="flex flex-row items-center">
-          <button className="flex flex-row  items-center">
-            <p className="font-mulish text-white text-lg m-2">Emil Emilsson</p>
-            <KeyboardArrowDownIcon
-              className="w-5 h-5"
-              style={{ color: "white" }}
-            />
-          </button>
-        </div>
-      </div>
+      <ProfilePreview name="Emil Emilsson" image={LoginImg}/>
     </div>
   );
 }
