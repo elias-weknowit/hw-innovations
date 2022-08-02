bf9fff4a0e1253cff298ffd4db64fe7032c025dc
Reza Parvar <reza.parvar@weknowit.nu>
2022-08-02 11:50:58 +0200

    Added new component for skills section

--- components/Profile/Skills/SkillsComponent.tsx
+++ components/Profile/Skills/SkillsComponent.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import BorderColorOutlinedIcon from "@mui/icons-material/BorderColorOutlined";
 import MilitaryTechOutlinedIcon from "@mui/icons-material/MilitaryTechOutlined";
 import { Divider } from "@mui/material";
+import Skills from "./Skills";
 
-export default function Skills() {
+export default function SkillsComponent() {
   return (
     <div className="flex flex-col">
       <div className="flex flex-row items-center justify-between p-3">
@@ -22,8 +23,10 @@ export default function Skills() {
         </div>
       </div>
       <Divider variant="middle" />
-      <div className="p-3 ml-6">
-        Massa boxar här genom att mapa ur alla färdigheter.
+      <div className="p-3 ml-6 flex flex-wrap">
+        <Skills skillName="Rörmokare" />
+        <Skills skillName="Rörmokare" />
+        <Skills skillName="Rörmokare" />
       </div>
     </div>
   );
