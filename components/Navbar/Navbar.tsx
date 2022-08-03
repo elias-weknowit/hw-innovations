b01980731201d5c038a14b79bb6a29bcb0322d25
Reza Parvar <reza.parvar@weknowit.nu>
2022-08-03 19:43:19 +0200

    Fixed responsive navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,41 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import logo from "../../public/Logo.svg";
 import Link from "next/link";
 import ProfilePreview from "./ProfilePreview";
 import LoginImg from "../../public/LoginImg.png";
 import { useAuth } from "../../components/firebase/AuthUserProvider";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 export default function Navbar({ transparent }: { transparent?: boolean }) {
   const { user } = useAuth();
 
+  const [open, setOpen] = useState(false);
+
   return (
     <div
       className={
-        "flex flex-row justify-between items-center h-20 " +
-        (transparent ? "" : "bg-primary-color")
+        "md:flex md:flex-row justify-between items-center h-20 w-full " +
+        (transparent ? "" : "bg-primary-color shadow-lg")
       }
     >
-      <div className="md:m-32">
-        <Image src={logo} width={171} height={56} />
+      <div className="flex flex-row items-center">
+        <div className="flex items-center justify-start p-1 md:m-32">
+          <Image src={logo} width={171} height={56} />
+        </div>
+        <div
+          className="flex flex-row items-center"
+          onClick={() => setOpen(!open)}
+        >
+          {open ? (
+            <CloseIcon
+              className="w-10 h-10 absolute right-3 cursor-pointer md:hidden"
+              style={{ color: "white" }}
+            />
+          ) : (
+            <MenuIcon
+              className="w-10 h-10 absolute right-3 cursor-pointer md:hidden"
+              style={{ color: "white" }}
+            />
+          )}
+        </div>
       </div>
-
-      <div className="flex flex-row">
+      <div
+        className={`md:flex md:flex-row md:z-auto w-full md:w-auto translate-all duration-300 ease-in ${
+          open
+            ? "flex flex-col items-center  mt-1 w-full"
+            : "flex flex-col items-center mt-1 md:opacity-100 w-full opacity-0"
+        }`}
+      >
         <Link href={"/login"}>
-          <p className="font-mulish md:mr-10 text-white cursor-pointer">Hem</p>
+          <p className="font-mulish md:mr-10 text-white cursor-pointer  ">
+            Hem
+          </p>
         </Link>
         <Link href={""}>
-          <p className="font-mulish md:mr-10 text-white cursor-pointer">
+          <p className="font-mulish md:mr-10 text-white cursor-pointer ">
             Annonser
           </p>
         </Link>
         <Link href={""}>
-          <p className="font-mulish md:mr-10 text-white cursor-pointer">
+          <p className="font-mulish md:mr-10 text-white cursor-pointer ">
             Skapa annons
           </p>
         </Link>
       </div>
-      <ProfilePreview user={user} image={LoginImg} />
+      <div
+        className={`md:flex md:flex-row md:z-auto w-full md:w-auto translate-all duration-300 ease-in ${
+          open
+            ? "flex flex-col items-center w-full "
+            : "flex flex-col items-center md:opacity-100 w-full  opacity-0 "
+        }`}
+      >
+        <ProfilePreview user={user} image={LoginImg} />
+      </div>
     </div>
   );
 }
--- components/Navbar/ProfilePreview.tsx
+++ components/Navbar/ProfilePreview.tsx
@@ -3,46 +3,43 @@ import { User } from "firebase/auth";
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import { useAuth } from "../firebase/AuthUserProvider";
-
+import DropDown from "./DropDown";
 
 interface ProfilePreviewProps {
-    user: User | null;
-    image: StaticImageData;
+  user: User | null;
+  image: StaticImageData;
 }
 
-export default function ProfilePreview({user, image}: ProfilePreviewProps) {
-    const { signOut } = useAuth();
-    return (
-        <div className="md:m-32 flex flex-row items-center justify-self-end">
-            {user ?
-            <>
-                <div className="flex bg-black bg-opacity-40 rounded-full w-11 h-11 items-center justify-center text-white overflow-hidden">
-                    <Image src={image} width="100%" height="100%" />
-                </div>
-                <div className="flex flex-row items-center">
-                    <Link href="/user-profile">
-                        <a className="font-mulish text-white text-lg m-2">
-                            {user.email}
-                        </a>
-                    </Link>
-                    <button onClick={signOut}>
-                        <KeyboardArrowDownIcon className="w-5 h-5" style={{ color: "white" }}/>
-                    </button>
-                </div>
-            </>
-            :
-            <>
-                <div className="flex flex-row items-center">
-                    <Link href="/login">
-                        <a className="font-mulish text-white text-lg m-2">
-                            Logga in
-                        </a>
-                    </Link>
-                </div>
-            </>
-            }
-            
-            
-        </div>
-    )
+export default function ProfilePreview({ user, image }: ProfilePreviewProps) {
+  const { signOut } = useAuth();
+  return (
+    <div className="md:m-32 flex flex-row items-center justify-self-end">
+      {user ? (
+        <>
+          <div className="flex bg-black bg-opacity-40 rounded-full w-11 h-11 items-center justify-center text-white overflow-hidden">
+            <Image src={image} width="100%" height="100%" />
+          </div>
+          <div className="flex flex-row items-center">
+            <Link href="/user-profile">
+              <a className="font-mulish text-white text-lg m-2">{user.email}</a>
+            </Link>
+            <button onClick={signOut}>
+              <KeyboardArrowDownIcon
+                className="w-5 h-5"
+                style={{ color: "white" }}
+              />
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="flex flex-row items-center">
+            <Link href="/login">
+              <a className="font-mulish text-white text-lg">Logga in</a>
+            </Link>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
