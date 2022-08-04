cdf21771ed637e81501e063c42e7a3056268fcd5
Reza Parvar <reza.parvar@weknowit.nu>
2022-08-04 23:50:34 +0200

    Fixed responsive and added user img to profile page

--- components/Profile/UserPresentation.tsx
+++ components/Profile/UserPresentation.tsx
@@ -1,17 +1,29 @@
+import Image, { StaticImageData } from "next/image";
+import { User } from "firebase/auth";
 import React from "react";
 
-export default function UserPresentation({ userName, userPosition }) {
+interface UserPresentationProps {
+  image: string | StaticImageData;
+  username: string;
+  userPosition: string;
+}
+
+export default function UserPresentation({
+  image,
+  username,
+  userPosition,
+}: UserPresentationProps) {
   return (
     <div className="flex flex-row items-center justify-start w-full">
-      <div className="flex flex-col md:flex-row items-start md:items-center bg-primary-color">
-        <div className="bg-black bg-opacity-30 p-10 md:p-16 rounded-full m-4 text-white">
-          Img
+      <div className="flex flex-col md:flex-row items-start md:items-center">
+        <div className="bg-success bg-opacity-30 p-2 lg:p-4 rounded-full m-1 text-white lg:flex-shrink-0">
+          <Image src={image} width="100%" height="100%" />
         </div>
-        <div className="flex flex-col justify-center p-4 md:p-3">
-          <p className="sm:text-lg md:text-4xl font-mulish text-white">
-            {userName}
+        <div className="flex flex-col justify-center p-4">
+          <p className="text-lg lg:text-4xl font-mulish text-white">
+            {username}
           </p>
-          <p className="text-sm opacity-80 md:mt-4 font-mulish text-white">
+          <p className="text-sm opacity-80 lg:mt-4 font-mulish text-white">
             {userPosition}
           </p>
         </div>
