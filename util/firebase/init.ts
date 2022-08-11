4f1c5bba15e3c70869625198f6118278554dbcf1
rasmuslyxell <rasmus.lyxell@weknowit.se>
2022-08-11 11:59:42 +0200

    Remove unneccesary code

--- util/firebase/init.ts
+++ util/firebase/init.ts
@@ -1,18 +1,16 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword, browserSessionPersistence} from "firebase/auth";
-import { useEffect } from "react";
-import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { initializeApp, getApps} from "firebase/app";
+import { getAuth, browserSessionPersistence} from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
-  };
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+};
 
 export function initFirebase(): boolean{
     // if a Firebase instance doesn't exist, create one
@@ -20,10 +18,6 @@ export function initFirebase(): boolean{
 
   //Return result of initliazing Firebase
   return getApps().length > 0;
-}
-
-export function initFirestore(): boolean {
-  return false;
 }
 
 export function initAuth(): boolean {
