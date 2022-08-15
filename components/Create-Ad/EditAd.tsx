ddb2955457e423fe0051cf519ccef2739cd7f8e5
Reza Parvar <reza.parvar@weknowit.nu>
2022-08-15 22:42:44 +0200

    Added actuall data

--- components/Create-Ad/EditAd.tsx
+++ components/Create-Ad/EditAd.tsx
@@ -6,24 +6,25 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { Advertisement } from "../../util/models";
+import { useState } from "react";
+import moment from "moment";
 
-export default function EditAd() {
+export default function EditAd({
+  ad,
+  onSubmit,
+  onDelete,
+}: {
+  ad: Advertisement;
+  onSubmit: (ad: Advertisement) => void;
+  onDelete: () => void;
+}) {
   const router = useRouter();
-
-  const goBack = (e) => {
-    e.preventDefault();
-    router.push("#");
-  };
+  const [adData, setAdData] = useState({ ...ad });
 
   return (
     <>
       <div className="flex flex-col bg-profile-sections md:w-full md:ml-2 lg:ml-2 shadow-md rounded-3xl p-4 mt-3 mb-10">
-        {/*<div
-          className="flex justify-start items-start md:hidden lg:hidden"
-          onClick={goBack}
-        >
-          <ArrowBackIcon className="w-7 h-7" />
-        </div>*/}
         {/**Tile */}
         <div className="flex flex-col justify-start mb-6">
           <p className="mt-4 text-3xl md:text-xl lg:text-xl font-mulish font-semibold">
@@ -32,65 +33,143 @@ export default function EditAd() {
         </div>
         <div>
           <form>
-            <RadioButton />
+            <RadioButton
+              name="workType"
+              labels={["Jobb", "Arbetskraft"]}
+              title="Erbjuder"
+              onChange={(label) => {}}
+            />
             <InputForm
-              labelName="Jobbtitel"
+              labelName="Rubrik"
               type
               date="text"
-              value={null}
-              onChange={null}
+              value={adData.title}
+              onChange={(e) => setAdData({ ...adData, title: e.target.value })}
             />
             <InputForm
               labelName="Företag"
               type
               date="text"
-              value={null}
-              onChange={null}
+              value={adData.company}
+              onChange={(e) =>
+                setAdData({ ...adData, company: e.target.value })
+              }
             />
             <InputForm
               labelName="Plats"
               type
               date="text"
-              value={null}
-              onChange={null}
+              value={adData.location}
+              onChange={(e) =>
+                setAdData({ ...adData, location: e.target.value })
+              }
             />
             <InputForm
-              labelName="När"
+              labelName="Period"
               type
               date="date"
-              value={null}
-              onChange={null}
+              value={""}
+              //value={adData.period.start.format("YYY-MM-DD")}
+              onChange={
+                () => {} /*(e) =>
+                setAdData({
+                  ...adData,
+                  period: { ...adData.period, start: moment(e.target.value) },
+                })*/
+              }
+            />
+            <InputForm
+              labelName="Antal personer"
+              type
+              date="text"
+              value={adData.amount.toString()}
+              onChange={(e) =>
+                setAdData({ ...adData, amount: parseInt(e.target.value) })
+              }
+            />
+            <RadioButton
+              title="Ansluten till kollektivavtal"
+              name="collectiveAgreement"
+              labels={["Ja", "Nej"]}
+              onChange={(label) => {}}
+            />
+            <InputForm
+              labelName="Avtalsform"
+              type
+              date="text"
+              value={adData.contractForm}
+              onChange={(e) =>
+                setAdData({ ...adData, contractForm: e.target.value })
+              }
             />
             <InputForm
               labelName="Typ av arbetskraft"
               type
               date="text"
-              value={null}
-              onChange={null}
-            />
-            <InputForm
-              labelName="Antal"
-              type
-              date="text"
-              value={null}
-              onChange={null}
+              value={adData.typeOfWork}
+              onChange={(e) =>
+                setAdData({ ...adData, typeOfWork: e.target.value })
+              }
             />
             <InputForm
               labelName="Beskrivning"
               date="text"
-              value={null}
-              onChange={null}
+              value={adData.description}
+              onChange={(e) =>
+                setAdData({ ...adData, description: e.target.value })
+              }
             />
             <InputForm
               labelName="Kvalifikationer"
               date="text"
-              value={null}
-              onChange={null}
+              value={adData.requirements.join("\n")}
+              onChange={(e) =>
+                setAdData({
+                  ...adData,
+                  requirements: e.target.value.split("\n"),
+                })
+              }
             />
-            <UploadImgForm />
+            <InputForm
+              labelName="Kontaktperson"
+              type
+              date="text"
+              value={adData.contact.name}
+              onChange={(e) =>
+                setAdData({
+                  ...adData,
+                  contact: { ...adData.contact, name: e.target.value },
+                })
+              }
+            />
+            <InputForm
+              labelName="Telefonnummer"
+              type
+              date="text"
+              value={adData.contact.phone}
+              onChange={(e) =>
+                setAdData({
+                  ...adData,
+                  contact: { ...adData.contact, phone: e.target.value },
+                })
+              }
+            />
+            <InputForm
+              labelName="Adress"
+              type
+              date="text"
+              value={adData.contact.address}
+              onChange={(e) =>
+                setAdData({
+                  ...adData,
+                  contact: { ...adData.contact, address: e.target.value },
+                })
+              }
+            />
+            <UploadImgForm className="shadow-sm p-1 md:p-2 rounded-md font-mulish w-1/2" />
             <div className="flex flex-row items-center justify-between mt-14">
               <div className="">
-                <button className="flex items-center">
+                <button className="flex items-center" onClick={onDelete}>
                   <DeleteIcon
                     className="h-5 w-5"
                     style={{ color: "#EB363D" }}
@@ -102,8 +181,11 @@ export default function EditAd() {
               </div>
               <div>
                 <button className="bg-primary-color p-1 rounded-md">
-                  <div className="font-mulish font-semibold text-white mx-10">
-                    <Link href={""}>Spara</Link>
+                  <div
+                    className="font-mulish font-semibold text-white mx-10"
+                    onClick={() => onSubmit(adData)}
+                  >
+                    Spara
                   </div>
                 </button>
               </div>
