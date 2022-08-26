167f5ec864eb260c217218878a7d56c3bfeee842
Reza Parvar <reza.parvar@weknowit.nu>
2022-08-26 09:54:14 +0200

    Added a new page for "Annonser"

---
+++ pages/job.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Head from "next/head";
+import SearchBar from "../components/Ad/SearchBar";
+import FilterButton from "../components/Ad/FilterButton";
+import AdDetail from "../components/Ad/AdDetail";
+import FilterView from "../components/Ad/FilterView";
+import AdView from "../components/Create-Ad/AdView";
+
+export default function Job() {
+  return (
+    <>
+      <Head>
+        <title>Work 2 Do | Annonser</title>
+      </Head>
+      <div className="px-8 sm:px-12 md:px-16 lg:px-32 mt-40 md:mt-20">
+        <div>
+          <div className="flex flex-row items-center justify-center">
+            <SearchBar placeholder="Sök efter jobb" />
+            <FilterButton />
+          </div>
+          <div className="flex items-center justify-between mt-10">
+            <div className="block md:hidden font-mulish font-semibold text-md mb-2">
+              Nya Jobb
+            </div>
+            <div className="block md:hidden">Toggle button</div>
+          </div>
+        </div>
+        <div className="md:flex mb-5">
+          <div className="hidden md:block md:w-1/4">
+            <FilterView />
+          </div>
+          <div className="md:w-1/3 p-1">
+            <div>
+              <AdDetail />
+              <AdDetail />
+              <AdDetail />
+              <AdDetail />
+            </div>
+          </div>
+          <div className="hidden md:block md:w-1/2 p-1">
+            AdView ska mapas här. Hur?
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
