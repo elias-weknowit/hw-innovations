aec9f7d3a498fd38cbc7b02694b08c79b2043d5f
Reza Parvar <reza.parvar@weknowit.nu>
2022-08-26 09:58:32 +0200

    Added prop and changed structure

--- components/Create-Ad/components/RadioButton.tsx
+++ components/Create-Ad/components/RadioButton.tsx
@@ -7,6 +7,8 @@ interface RadioButtonProps {
   onChange: (e) => void;
 }
 
+/**onChange={(e) => onChange((e.target as HTMLInputElement).value)} */
+
 export default function RadioButton({
   title,
   onChange,
@@ -14,17 +16,18 @@ export default function RadioButton({
   labels,
 }: RadioButtonProps) {
   return (
-    <div onChange={(e) => onChange((e.target as HTMLInputElement).value)}>
-      <p className="font-mulish font-semibold mb-1">{title}</p>
+    <div>
+      <p className="font-mulish font-semibold mb-5">{title}</p>
       {labels.map((label, index) => {
         return (
           <div className="flex items-center mb-5">
             <input
-              className="w-5 h-5 border-primary-color focus:ring-primary-color focus:ring-1"
+              className="w-5 h-5 border-primary-color accent-primary-color focus:ring-primary-color focus:ring-1"
               type="radio"
               value={label}
               defaultChecked={index === 0}
               name={name}
+              onChange={onChange}
             />
             <label className="ml-4 text-md font-mulish text-primary-color">
               {label}
