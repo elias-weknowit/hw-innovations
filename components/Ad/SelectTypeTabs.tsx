ff4e035dc04493d0636c7c25d57bce68fc82b682
rasmuslyxell <rasmus.lyxell@weknowit.se>
2022-09-30 10:58:05 +0200

    Created component for toggle button

---
+++ components/Ad/SelectTypeTabs.tsx
@@ -0,0 +1,28 @@
+
+export interface SelectTypeTabsProps {
+    tabs: string[];
+    onSelect: (tab: string) => void;
+    selectedTab: string;
+    className?: string;
+}
+
+export default function SelectTypeTabs({ tabs, selectedTab, onSelect, className = "" }: SelectTypeTabsProps) {
+  return (
+    <div className={className + " flex flex-row justify-center items-center"}>
+      {tabs.map((tab, idx) => (
+        <div
+            key={tab}
+            className={`flex  flex-grow flex-shrink-0 basis-0 flex-row justify-center items-center cursor-pointer ${
+            tab === selectedTab
+              ? "bg-primary-color text-white"
+              : "bg-white text-primary-color"
+          }
+          ${idx === 0 ? "rounded-l-3xl" : ""} ${idx === tabs.length - 1 ? "rounded-r-3xl" : ""}`}
+          onClick={() => onSelect(tab)}
+        >
+          <p className="font-mulish font-semibold p-2">{tab}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
